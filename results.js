d3941867eb46ef8290afc761365f036d4613b1af
pabloang28 <angulo.aldeguer.pablo@gmail.com>
2026-06-24 15:47:35 +0200

    Update results.js

--- results.js
+++ results.js
@@ -3,59 +3,78 @@ const RESULTS = {
   thirdPlace: [],
  groupMatches: {
     "A": {
-      "Mexico__South Africa": { outcome: "home" },
-      "Czech Republic__South Korea": { outcome: "home" },
-      "Czech Republic__South Africa": { outcome: "draw" },
-      "Mexico__South Korea": { outcome: "home" }
+      "Mexico__South Africa": { "outcome": "home" },
+      "Czech Republic__South Korea": { "outcome": "home" },
+      "Czech Republic__South Africa": { "outcome": "draw" },
+      "Mexico__South Korea": { "outcome": "home" }
     },
     "B": {
-      "Canada__Bosnia & Herzegovina": { outcome: "draw" },
-      "Qatar__Switzerland": { outcome: "draw" },
-      "Switzerland__Bosnia & Herzegovina": { outcome: "home" },
-      "Canada__Qatar": { outcome: "home" }
+      "Canada__Bosnia & Herzegovina": { "outcome": "draw" },
+      "Qatar__Switzerland": { "outcome": "draw" },
+      "Switzerland__Bosnia & Herzegovina": { "outcome": "home" },
+      "Canada__Qatar": { "outcome": "home" }
     },
     "C": {
-      "Brazil__Morocco": { outcome: "draw" },
-      "Haiti__Scotland": { outcome: "away" }
+      "Brazil__Morocco": { "outcome": "draw" },
+      "Haiti__Scotland": { "outcome": "away" },
+      "Brazil__Haiti": { "outcome": "home" },
+      "Morocco__Scotland": { "outcome": "away" }
     },
     "D": {
-      "Paraguay__USA": { outcome: "home" },
-      "Australia__Turkey": { outcome: "home" },
-      "USA_Australia": { outcome: "home" }
+      "Paraguay__USA": { "outcome": "home" },
+      "Australia__Turkey": { "outcome": "home" },
+      "USA__Australia": { "outcome": "home" },
+      "Turkey__Paraguay": { "outcome": "away" }
     },
     "E": {
-      "Germany__Curaçao": { outcome: "home" },
-      "Ivory Coast__Ecuador": { outcome: "home" }
+      "Germany__Curaçao": { "outcome": "home" },
+      "Ivory Coast__Ecuador": { "outcome": "home" },
+      "Germany__Ivory Coast": { "outcome": "home" },
+      "Curaçao__Ecuador": { "outcome": "draw" }
     },
- "F": {
-      "Netherlands__Japan": { outcome: "draw" },
-      "Sweden__Tunisia": { outcome: "home" }
-    },
-    "H": {
-      "Spain__Cape Verde": { outcome: "draw" },
-      "Saudi Arabia__Uruguay": { outcome: "draw" }
+    "F": {
+      "Netherlands__Japan": { "outcome": "draw" },
+      "Sweden__Tunisia": { "outcome": "home" },
+      "Netherlands__Sweden": { "outcome": "home" },
+      "Japan__Tunisia": { "outcome": "away" }
     },
     "G": {
-      "Belgium__Egypt": { outcome: "draw" },
-      "Iran__New Zealand": { outcome: "draw" }
+      "Belgium__Egypt": { "outcome": "draw" },
+      "Iran__New Zealand": { "outcome": "draw" },
+      "Belgium__Iran": { "outcome": "draw" },
+      "Egypt__New Zealand": { "outcome": "away" }
+    },
+    "H": {
+      "Spain__Cape Verde": { "outcome": "draw" },
+      "Saudi Arabia__Uruguay": { "outcome": "draw" },
+      "Spain__Saudi Arabia": { "outcome": "home" },
+      "Cape Verde__Uruguay": { "outcome": "draw" }
     },
     "I": {
-      "France__Senegal": { outcome: "home" },
-      "Iraq__Norway": { outcome: "away" }
+      "France__Senegal": { "outcome": "home" },
+      "Iraq__Norway": { "outcome": "away" },
+      "France__Iraq": { "outcome": "home" },
+      "Senegal__Norway": { "outcome": "home" }
     },
     "J": {
-      "Argentina__Algeria": { outcome: "home" },
-      "Austria__Jordan": { outcome: "home" }
-         },
-      "K": {
-      "Portugal__DR Congo": { outcome: "draw" },
-      "Uzbekistan__Colombia": { outcome: "away" }
-         },
-     "L": {
-      "England__Croatia": { outcome: "home" },
-      "Ghana__Panama": { outcome: "home" }
-         }
-  }, 
+      "Argentina__Algeria": { "outcome": "home" },
+      "Austria__Jordan": { "outcome": "home" },
+      "Argentina__Austria": { "outcome": "home" },
+      "Algeria__Jordan": { "outcome": "away" }
+    },
+    "K": {
+      "Portugal__DR Congo": { "outcome": "draw" },
+      "Uzbekistan__Colombia": { "outcome": "away" },
+      "DR Congo__Colombia": { "outcome": "home" },
+      "Portugal__Uzbekistan": { "outcome": "home" }
+    },
+    "L": {
+      "England__Croatia": { "outcome": "home" },
+      "Ghana__Panama": { "outcome": "home" },
+      "England__Ghana": { "outcome": "draw" },
+      "Croatia__Panama": { "outcome": "away" }
+    }
+},
   knockout: {
     round32: [],
     round16: [],
