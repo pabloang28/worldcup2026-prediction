f665579da93aacbae9a4fd298cc5b2ced0e5d665
pabloang28 <angulo.aldeguer.pablo@gmail.com>
2026-06-29 07:45:15 +0200

    Update results.js

--- results.js
+++ results.js
@@ -5,9 +5,24 @@ const RESULTS = {
      "C": ["Brazil","Morocco", "Scotland", "Haiti"],
      "D": ["USA","Australia", "Paraguay", "Turkey"],
      "E": ["Germany","Ivory Coast", "Ecuador", "Curaçao"],
-     "F": ["Netherlands","Japan", "Sweden", "Tunisia"]
+     "F": ["Netherlands","Japan", "Sweden", "Tunisia"],
+     "G": ["Belgium","Egypt", "Iran", "New Zealand"],
+     "H": ["Spain","Cape Verde", "Uruguay", "Saudi Arabia"],
+     "I": ["France","Norway", "Senegal", "Iraq"],
+     "J": ["Argentina","Austria", "Algeria", "Jordan"],
+     "K": ["Colombia","Portugal", "DR Congo", "Uzbekistan"],
+     "L": ["England","Croatia", "Ghana", "Panama"]    
   },
-  thirdPlace: [],
+  thirdPlace: [
+    "DR Congo", 
+    "Sweden", 
+    "Ghana", 
+    "Ecuador", 
+    "Bosnia & Herzegovina", 
+    "Algeria", 
+    "Paraguay", 
+    "Senegal"
+],
  groupMatches: {
     "A": {
       "Mexico__South Africa": { "outcome": "home" },
@@ -61,41 +76,53 @@ const RESULTS = {
       "Belgium__Egypt": { "outcome": "draw" },
       "Iran__New Zealand": { "outcome": "draw" },
       "Belgium__Iran": { "outcome": "draw" },
-      "Egypt__New Zealand": { "outcome": "away" }
+      "Egypt__New Zealand": { "outcome": "away" },
+      "Egypt__Iran": { "outcome": "draw" },
+      "New Zealand__Belgium": { "outcome": "away" }
     },
     "H": {
       "Spain__Cape Verde": { "outcome": "draw" },
       "Saudi Arabia__Uruguay": { "outcome": "draw" },
       "Spain__Saudi Arabia": { "outcome": "home" },
-      "Cape Verde__Uruguay": { "outcome": "draw" }
+      "Cape Verde__Uruguay": { "outcome": "draw" },
+      "Uruguay__Spain": { "outcome": "away" },
+      "Cape Verde__Saudi Arabia": { "outcome": "draw" }
     },
     "I": {
       "France__Senegal": { "outcome": "home" },
       "Iraq__Norway": { "outcome": "away" },
       "France__Iraq": { "outcome": "home" },
-      "Senegal__Norway": { "outcome": "home" }
+      "Senegal__Norway": { "outcome": "home" },
+      "Norway__France": { "outcome": "away" },
+      "Senegal__Iraq": { "outcome": "home" }
     },
     "J": {
       "Argentina__Algeria": { "outcome": "home" },
       "Austria__Jordan": { "outcome": "home" },
       "Argentina__Austria": { "outcome": "home" },
-      "Algeria__Jordan": { "outcome": "away" }
+      "Algeria__Jordan": { "outcome": "away" },
+      "Algeria__Austria": { "outcome": "draw" },
+      "Jordan__Argentina": { "outcome": "away" }
     },
     "K": {
       "Portugal__DR Congo": { "outcome": "draw" },
       "Uzbekistan__Colombia": { "outcome": "away" },
       "DR Congo__Colombia": { "outcome": "home" },
-      "Portugal__Uzbekistan": { "outcome": "home" }
+      "Portugal__Uzbekistan": { "outcome": "home" },
+      "Colombia__Portugal": { "outcome": "draw" },
+      "DR Congo__Uzbekistan": { "outcome": "home" }
     },
     "L": {
       "England__Croatia": { "outcome": "home" },
       "Ghana__Panama": { "outcome": "home" },
       "England__Ghana": { "outcome": "draw" },
-      "Croatia__Panama": { "outcome": "away" }
+      "Croatia__Panama": { "outcome": "away" },
+      "Panama__England": { "outcome": "away" },
+      "Croatia__Ghana": { "outcome": "home" }
     }
 },
   knockout: {
-    round32: [],
+    round32: ["South Africa", "Canada","Netherlands", "Morocco", "Germany", "Paraguay", "France", "Sweden", "Belgium", "Senegal", "USA", "Bosnia & Herzegovina", "Spain", "Austria", "Portugal","Croatia","Brazil", "Japan","Ivory Coast","Norway", "Mexico", "Ecuador","England","DR Congo","Switzerland","Algeria","Colombia","Ghana","Australia","Egypt", "Argentina", "Cape Verde" ],
     round16: [],
     quarterfinals: [],
     semifinals: [],
@@ -107,12 +134,12 @@ const RESULTS = {
     thirdPlace: "",
     matches: {
       round32: [
-        // {
-        //   match: 73,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
+        {
+          match: 73,
+          team1: "South Africa",
+          team2: "Canada",
+          winner: "Canada"
+        }
       ],
       round16: [
         // {
